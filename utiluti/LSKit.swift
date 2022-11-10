bec420bd56d4c506e5d406725cc22774a056c932
Armin Briegel <1933192+scriptingosx@users.noreply.github.com>
2022-11-10 09:33:16 +0100

    first commit

---
+++ utiluti/LSKit.swift
@@ -0,0 +1,179 @@
+//
+//  LSKit.swift
+//  LSKit
+//
+//  Created by Armin Briegel on 2021-08-30.
+//
+
+import Foundation
+import AppKit
+import UniformTypeIdentifiers
+
+class LSKit {
+  
+  /**
+   returns a list of URLs to applications that can open URLs starting with the scheme
+   - Parameter scheme: url scheme (excluding the `:` or `/`, e.g. `http`)
+   - Returns: array of app URLs
+   */
+  static func appURLs(forScheme scheme: String) -> [URL] {
+    guard let url = URL(string: "\(scheme):") else { return [URL]() }
+    
+    if #available(macOS 12, *) {
+      //print("running on macOS 12, using NSWorkspace")
+      let ws = NSWorkspace.shared
+      return ws.urlsForApplications(toOpen: url)
+    } else {
+      var urlList = [URL]()
+      if let result = LSCopyApplicationURLsForURL(url as CFURL, .all) {
+        let cfURLList = result.takeRetainedValue() as Array
+        for item in cfURLList {
+          if let appURL = item as? URL {
+            urlList.append(appURL)
+          }
+        }
+      }
+      return urlList
+    }
+  }
+  
+  /**
+   returns URL to the default application for URLs starting with scheme
+   - Parameter scheme: url scheme (excluding the `:` or `/`, e.g. `http`)
+   - Returns: urls to default application
+   */
+  static func defaultAppURL(forScheme scheme: String) -> URL? {
+    guard let url = URL(string: "\(scheme):") else { return nil }
+    
+    if #available(macOS 12, *) {
+      //print("running on macOS 12, using NSWorkspace")
+      let ws = NSWorkspace.shared
+      return ws.urlForApplication(toOpen: url)
+    } else {
+      if let result = LSCopyDefaultApplicationURLForURL(url as CFURL, .all, nil) {
+        let appURL = result.takeRetainedValue() as URL
+        return appURL
+      }
+      return nil
+    }
+  }
+  
+  /**
+   set the default app for scheme to the app with the identifier
+   - Parameters:
+   - identifier: bundle id of the new default application
+   - scheme: url scheme (excluding the `:` or `/`, e.g. `http`)
+   - Returns: OSStatus (discardable)
+   */
+  @discardableResult static func setDefaultApp(identifier: String, forScheme scheme: String) -> OSStatus {
+    if #available(macOS 12, *) {
+      // print("running on macOS 12, using NSWorkspace")
+      let ws = NSWorkspace.shared
+      
+      // since the new NSWorkspace function is asynchronous we have to use semaphores here
+      let semaphore = DispatchSemaphore(value: 0)
+      var errCode: OSStatus = 0
+      
+      guard let appURL = ws.urlForApplication(withBundleIdentifier: identifier) else { return 1 }
+      ws.setDefaultApplication(at: appURL, toOpenURLsWithScheme: scheme) { err in
+        // err is an NSError wrapped in a CocoaError
+        if let err = err as? CocoaError {
+          if let underlyingError = err.errorUserInfo["NSUnderlyingError"] as? NSError {
+            errCode = OSStatus(clamping: underlyingError.code)
+          }
+        }
+        semaphore.signal()
+      }
+      semaphore.wait()
+      return errCode
+    } else {
+      return LSSetDefaultHandlerForURLScheme(scheme as CFString, identifier as CFString)
+    }
+  }
+  
+  /**
+   returns a list of URLs to applications that can open the given type identifier
+   - Parameter forTypeIdentifier: Uniform Type Identifier, e.g. `public.html`
+   - Returns: array of URLs to apps
+   */
+  static func appURLs(forTypeIdentifier utidentifier: String) -> [URL] {
+    if #available(macOS 12, *) {
+      print("running on macOS 12, using NSWorkspace")
+      let ws = NSWorkspace.shared
+      guard let utype = UTType(utidentifier) else {
+        return [URL]()
+      }
+      return ws.urlsForApplications(toOpen: utype)
+    } else {
+      var urlList = [URL]()
+      if let result = LSCopyAllRoleHandlersForContentType(utidentifier as CFString, .all) {
+        let cfURLList = result.takeRetainedValue() as Array
+        for item in cfURLList {
+          if let appURL = item as? URL {
+            urlList.append(appURL)
+          }
+        }
+      }
+      return urlList
+    }
+  }
+  
+  /**
+   returns URL to the default application for the given type identifier
+   - Parameter forTypeIdentifier: url scheme (excluding the `:` or `/`, e.g. `http`)
+   - Returns: url to default application
+   */
+  static func defaultAppURL(forTypeIdentifier utidentifier: String) -> URL? {
+    if #available(macOS 12, *) {
+      //print("running on macOS 12, using NSWorkspace")
+      guard let utype = UTType(utidentifier) else {
+        return nil
+      }
+      let ws = NSWorkspace.shared
+      return ws.urlForApplication(toOpen: utype)
+    } else {
+      if let result = LSCopyDefaultApplicationURLForContentType(utidentifier as CFString, .all, nil) {
+        let appURL = result.takeRetainedValue() as URL
+        return appURL
+      }
+      return nil
+    }
+  }
+
+  /**
+   set the default app for type identifier to the app with the bundle indentifier
+   - Parameters:
+   - identifier: bundle id of the new default application
+   - forTypeIdentifier: uniform type identifier ( e.g. `public.html`)
+   - Returns: OSStatus (discardable)
+   */
+  @discardableResult static func setDefaultApp(identifier: String, forTypeIdentifier utidentifier: String) -> OSStatus {
+    if #available(macOS 12, *) {
+      // print("running on macOS 12, using NSWorkspace")
+      guard let utype = UTType(utidentifier) else {
+        return 1
+      }
+
+      let ws = NSWorkspace.shared
+      
+      // since the new NSWorkspace function is asynchronous we have to use semaphores here
+      let semaphore = DispatchSemaphore(value: 0)
+      var errCode: OSStatus = 0
+      
+      guard let appURL = ws.urlForApplication(withBundleIdentifier: identifier) else { return 1 }
+      ws.setDefaultApplication(at: appURL, toOpen: utype) { err in
+        // err is an NSError wrapped in a CocoaError
+        if let err = err as? CocoaError {
+          if let underlyingError = err.errorUserInfo["NSUnderlyingError"] as? NSError {
+            errCode = OSStatus(clamping: underlyingError.code)
+          }
+        }
+        semaphore.signal()
+      }
+      semaphore.wait()
+      return errCode
+    } else {
+      return LSSetDefaultRoleHandlerForContentType(utidentifier as CFString, .all, identifier as CFString)
+    }
+  }
+}
--- utiluti/main.swift
+++ utiluti/main.swift
@@ -6,6 +6,129 @@
 //
 
 import Foundation
+import ArgumentParser
 
-print("Hello, World!")
+struct UtilUTI: ParsableCommand {
+  static var configuration = CommandConfiguration(
+    commandName: "utiluti",
+    abstract: "Read and set default URL scheme and file type handlers.",
+    subcommands: [URLCommands.self, TypeCommands.self])
+  
+  struct URLCommands: ParsableCommand {
+    static var configuration = CommandConfiguration(
+      commandName: "url",
+      abstract: "Manipulate default URL scheme handlers",
+      subcommands: [ Get.self, List.self, Set.self],
+      defaultSubcommand: Get.self)
+    
+    struct URLScheme: ParsableArguments {
+      @Argument(help: "the url scheme, e.g. 'http' or 'mailto'") var urlScheme: String
+    }
+    
+    struct Get: ParsableCommand {
+      static var configuration
+      = CommandConfiguration(abstract: "Get the path to the default application.")
+      
+      @OptionGroup var args: URLScheme
+      
+      func run() {
+        let appURL = LSKit.defaultAppURL(forScheme: args.urlScheme)
+        print(appURL?.path ?? "no default app found")
+      }
+    }
+    
+    struct List: ParsableCommand {
+      static var configuration
+      = CommandConfiguration(abstract: "List all applications that can handle this URL scheme.")
+      
+      @OptionGroup var args: URLScheme
+      
+      func run() {
+        let appURLs = LSKit.appURLs(forScheme: args.urlScheme)
+        
+        for appURL in appURLs {
+          print(appURL.path)
+        }
+      }
+    }
+    
+    struct Set: ParsableCommand {
+      static var configuration
+      = CommandConfiguration(abstract: "Set the default app for this URL scheme.")
+      
+      @OptionGroup var args: URLScheme
+      @Argument(help: "bundle identifier for the app") var identifier: String
+      
+      func run() {
+        let result = LSKit.setDefaultApp(identifier: identifier, forScheme: args.urlScheme)
+        
+        if result == 0 {
+          print("set \(identifier) for \(args.urlScheme)")
+        } else {
+          print("cannot set default app for \(args.urlScheme) (error \(result))")
+        }
+      }
+    }
+  }
+  
+  struct TypeCommands: ParsableCommand {
+    static var configuration = CommandConfiguration(
+      commandName: "type",
+      abstract: "Manipulate default file type handlers",
+      subcommands: [Get.self, List.self, Set.self],
+      defaultSubcommand: Get.self
+    )
+    
+    struct UTIdentifier: ParsableArguments {
+      @Argument(help: "universal type identifier, e.g. 'public.html'") var utidentifier: String
+    }
+    
+    
+    struct Get: ParsableCommand {
+      static var configuration
+      = CommandConfiguration(abstract: "Get the path to the default application.")
+     
+      @OptionGroup var args: UTIdentifier
+      
+      func run() {
+        let appURL = LSKit.defaultAppURL(forTypeIdentifier: args.utidentifier)
+        print(appURL?.path ?? "no default app found")
+      }
+    }
+    
+    struct List: ParsableCommand {
+      static var configuration
+      = CommandConfiguration(abstract: "List all applications that can handle this type identifier.")
 
+      @OptionGroup var args: UTIdentifier
+      
+      func run() {
+        let appURLs = LSKit.appURLs(forTypeIdentifier: args.utidentifier)
+        
+        for appURL in appURLs {
+          print(appURL.path)
+        }
+      }
+    }
+    
+    struct Set: ParsableCommand {
+      static var configuration
+      = CommandConfiguration(abstract: "Set the default app for this type identifier.")
+
+      @OptionGroup var args: UTIdentifier
+      @Argument var identifier: String
+      
+      func run() {
+        let result = LSKit.setDefaultApp(identifier: identifier, forTypeIdentifier: args.utidentifier)
+        
+        if result == 0 {
+          print("set \(identifier) for \(args.utidentifier)")
+        } else {
+          print("cannot set default app for \(args.utidentifier) (error \(result))")
+        }
+      }
+    }
+  }
+}
+
+UtilUTI.main()
