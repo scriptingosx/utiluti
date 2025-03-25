390b76284d4942489e437ec1072d9891fdbbe076
Armin Briegel <1933192+scriptingosx@users.noreply.github.com>
2025-03-25 16:36:31 +0100

    improved output of 'app types'

--- Sources/utiluti/AppCommands.swift
+++ Sources/utiluti/AppCommands.swift
@@ -11,102 +11,108 @@ import UniformTypeIdentifiers
 import AppKit // for NSWorkspace
 
 struct AppCommands: ParsableCommand {
-    static let configuration = CommandConfiguration(
-        commandName: "app",
-        abstract: "list uniform types identifiers and url schemes associated with an app",
-        subcommands: [ Types.self, Schemes.self ]
-    )
+  static let configuration = CommandConfiguration(
+    commandName: "app",
+    abstract: "list uniform types identifiers and url schemes associated with an app",
+    subcommands: [ Types.self, Schemes.self ]
+  )
+  
+  struct Types: ParsableCommand {
+    static let configuration
+    = CommandConfiguration(abstract: "List the uniform type identifiers this app can open")
     
-    struct Types: ParsableCommand {
-        static let configuration
-        = CommandConfiguration(abstract: "List the uniform type identifiers this app can open")
-        
-        @Argument(help:ArgumentHelp("the app identifier", valueName: "app-identifier"))
-        var appID: String
-        
-        @Flag(name: .shortAndLong,
-              help: "show more information")
-        var verbose: Bool = false
-        
-        func run() {
-            guard
-                let appURL = NSWorkspace.shared.urlForApplication(withBundleIdentifier: appID),
-                let appBundle = Bundle(url: appURL),
-                let infoDictionary = appBundle.infoDictionary,
-                let docTypes: [[String: Any]] = infoDictionary["CFBundleDocumentTypes"] as? [[String: Any]]
-            else {
-                Self.exit(withError: ExitCode(7))
-            }
-            
-            for docType in docTypes {
-                guard
-                    let name = docType["CFBundleTypeName"] as? String
-                else { continue }
-              if let types = docType["LSItemContentTypes"] as? [String] {
-                for type in types {
-                  if verbose {
-                    print("\(type) - \(name)")
-                  } else {
-                    print(type)
-                  }
-                }
-              }
-              if let extensions = docType["CFBundleTypeExtensions"] as? [String] {
-                for ext in extensions {
-                  guard let utype = UTType(filenameExtension: ext) else {
-                    Self.exit(withError: ExitCode(3))
-                  }
-
-                  if utype.isDynamic {
-                    print("<unknown extension: \(ext)>")
-                  }
-
-                  if verbose {
-                    print("\(utype.identifier) - \(name)")
-                  } else {
-                    print(utype.identifier)
-                  }
-                }
-              }
-            }
-        }
-    }
+    @Argument(help:ArgumentHelp("the app identifier", valueName: "app-identifier"))
+    var appID: String
     
-    struct Schemes: ParsableCommand {
-        static let configuration
-        = CommandConfiguration(abstract: "List the urls schemes this app can open")
+    @Flag(name: .shortAndLong,
+          help: "show more information")
+    var verbose: Bool = false
+    
+    func run() {
+      guard
+        let appURL = NSWorkspace.shared.urlForApplication(withBundleIdentifier: appID),
+        let appBundle = Bundle(url: appURL),
+        let infoDictionary = appBundle.infoDictionary,
+        let docTypes: [[String: Any]] = infoDictionary["CFBundleDocumentTypes"] as? [[String: Any]]
+      else {
+        Self.exit(withError: ExitCode(7))
+      }
+      
+      for docType in docTypes {
+        guard
+          let name = docType["CFBundleTypeName"] as? String
+        else { continue }
         
-        @Argument(help:ArgumentHelp("the app identifier", valueName: "app-identifier"))
-        var appID: String
+        let types = docType["LSItemContentTypes"] as? [String] ?? []
+        let extensions = docType["CFBundleTypeExtensions"] as? [String] ?? []
         
-        @Flag(name: .shortAndLong,
-              help: "show more information")
-        var verbose: Bool = false
-        
-        func run() {
-            guard
-                let appURL = NSWorkspace.shared.urlForApplication(withBundleIdentifier: appID),
-                let appBundle = Bundle(url: appURL),
-                let infoDictionary = appBundle.infoDictionary,
-                let urlSchemes: [[String: Any]] = infoDictionary["CFBundleURLTypes"] as? [[String: Any]]
-            else {
-                Self.exit(withError: ExitCode(7))
-            }
-            
-            for docType in urlSchemes {
-                guard
-                    let name = docType["CFBundleURLName"] as? String,
-                    let schemes = docType["CFBundleURLSchemes"] as? [String]
-                else { continue }
-                for type in schemes {
-                    if verbose {
-                        print("\(type) - \(name)")
-                    } else {
-                        print(type)
-                    }
-                }
-            }
+        for type in types {
+          if verbose {
+            print("\(type) - \(name)")
+          } else {
+            print(type)
+          }
         }
+        
+        for ext in extensions {
+          guard let utype = UTType(filenameExtension: ext) else {
+            Self.exit(withError: ExitCode(3))
+          }
+          
+          if types.contains(utype.identifier) {
+            continue
+          }
+          
+          print("file extension: \(ext)", terminator: "")
+          
+          if !utype.isDynamic {
+            print(" (\(utype.identifier))", terminator: "")
+          }
+          
+          if verbose {
+            print(" - \(name)")
+          } else {
+            print()
+          }
+        }
+      }
     }
-
+  }
+  
+  struct Schemes: ParsableCommand {
+    static let configuration
+    = CommandConfiguration(abstract: "List the urls schemes this app can open")
+    
+    @Argument(help:ArgumentHelp("the app identifier", valueName: "app-identifier"))
+    var appID: String
+    
+    @Flag(name: .shortAndLong,
+          help: "show more information")
+    var verbose: Bool = false
+    
+    func run() {
+      guard
+        let appURL = NSWorkspace.shared.urlForApplication(withBundleIdentifier: appID),
+        let appBundle = Bundle(url: appURL),
+        let infoDictionary = appBundle.infoDictionary,
+        let urlSchemes: [[String: Any]] = infoDictionary["CFBundleURLTypes"] as? [[String: Any]]
+      else {
+        Self.exit(withError: ExitCode(7))
+      }
+      
+      for docType in urlSchemes {
+        guard
+          let name = docType["CFBundleURLName"] as? String,
+          let schemes = docType["CFBundleURLSchemes"] as? [String]
+        else { continue }
+        for type in schemes {
+          if verbose {
+            print("\(type) - \(name)")
+          } else {
+            print(type)
+          }
+        }
+      }
+    }
+  }
 }
