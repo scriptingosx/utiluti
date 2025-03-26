51cf4dd55ac7afeae8544dbaea180735e84159e4
Armin Briegel <1933192+scriptingosx@users.noreply.github.com>
2025-03-26 08:50:53 +0100

    added 'file app' and 'file set'

--- Sources/utiluti/FileCommands.swift
+++ Sources/utiluti/FileCommands.swift
@@ -13,10 +13,19 @@ import UniformTypeIdentifiers
 import AppKit // for NSWorkspace
 
 struct FileCommands: ParsableCommand {
+  
+  static var subCommands: [ParsableCommand.Type] {
+    if #available(macOS 12.0, *) {
+      return [ GetUTI.self, App.self, ListApps.self, Set.self ]
+    } else {
+      return [ GetUTI.self, App.self ]
+    }
+  }
+
   static let configuration = CommandConfiguration(
     commandName: "file",
     abstract: "commands to manage specific files",
-    subcommands: [ GetUTI.self ]
+    subcommands: subCommands
   )
   
   struct GetUTI: ParsableCommand {
@@ -30,6 +39,87 @@ struct FileCommands: ParsableCommand {
       let url = URL(fileURLWithPath: path)
       let typeIdentifier = try? url.resourceValues(forKeys: [.typeIdentifierKey]).typeIdentifier
       print(typeIdentifier ?? "<unknown>")
+    }
+  }
+  
+  struct App: ParsableCommand {
+    static let configuration
+    = CommandConfiguration(abstract: "get the app that will open this file")
+    
+    @Argument(help:ArgumentHelp("file path", valueName: "path"))
+    var path: String
+    
+    @Flag(help: ArgumentHelp(
+      "list bundle identifiers instead of paths",
+      valueName: "bundleID"))
+    var bundleID = false
+    
+    func run() {
+      let url = URL(fileURLWithPath: path)
+      if let app = NSWorkspace.shared.urlForApplication(toOpen: url) {
+        if bundleID {
+          guard let appBundle = Bundle(url: app) else {
+            Self.exit(withError: ExitCode(6))
+          }
+          print(appBundle.bundleIdentifier ?? "<no identifier>")
+        } else {
+          print(app.path)
+        }
+      } else {
+        print("no app found")
+        Self.exit(withError: ExitCode(9))
+      }
+    }
+  }
+  
+  @available(macOS 12, *)
+  struct ListApps: ParsableCommand {
+    static let configuration
+    = CommandConfiguration(abstract: "get all app that can open this file")
+    
+    @Argument(help:ArgumentHelp("file path", valueName: "path"))
+    var path: String
+    
+    @Flag(help: ArgumentHelp(
+      "list bundle identifiers instead of paths",
+      valueName: "bundleID"))
+    var bundleID = false
+    
+    func run() {
+      let url = URL(fileURLWithPath: path)
+      let apps = NSWorkspace.shared.urlsForApplications(toOpen: url)
+      for app in apps {
+        if bundleID {
+          guard let appBundle = Bundle(url: app) else {
+            Self.exit(withError: ExitCode(6))
+          }
+          print(appBundle.bundleIdentifier ?? "<no identifier>")
+        } else {
+          print(app.path)
+        }
+      }
+    }
+  }
+  
+  @available(macOS 12, *)
+  struct Set: AsyncParsableCommand {
+    static let configuration
+    = CommandConfiguration(abstract: "Set the default app for this specific file")
+    
+    @Argument(help:ArgumentHelp("file path", valueName: "path"))
+    var path: String
+    
+    @Argument(help: "the bundle identifier for the new default app")
+    var identifier: String
+
+    func run() async throws {
+      let url = URL(fileURLWithPath: path)
+      guard let appURL = NSWorkspace.shared.urlForApplication(withBundleIdentifier: identifier)
+      else {
+        Self.exit(withError: ExitCode(11))
+      }
+      try await NSWorkspace.shared.setDefaultApplication(at: appURL, toOpenFileAt: url)
+      print("set \(identifier) for \(path)")
     }
   }
 }
--- Sources/utiluti/utiluti.swift
+++ Sources/utiluti/utiluti.swift
@@ -5,13 +5,13 @@ import Foundation
 import ArgumentParser
 
 @main
-struct UtilUTI: ParsableCommand {
+struct UtilUTI: AsyncParsableCommand {
   static let subCommands: [ParsableCommand.Type] = [URLCommands.self, TypeCommands.self, GetUTI.self, AppCommands.self, FileCommands.self]
   
   static let configuration = CommandConfiguration(
     commandName: "utiluti",
     abstract: "Read and set default URL scheme and file type handlers.",
-    version: "1.1",
+    version: "1.1dev",
     subcommands: subCommands
   )
 }
