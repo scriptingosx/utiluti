a86ee36c17968339ef1461ceda1dced817e74f97
Dave DeLong <davedelong@me.com>
2025-07-16 08:29:04 -0600

    Don't use semaphores for blocking
    Methods with completion handlers, like the ones used on NSWorkspace, get imported as async methods to Swift Concurrency. This requires updating command definitions to be async.

--- Sources/utiluti/TypeCommands.swift
+++ Sources/utiluti/TypeCommands.swift
@@ -9,7 +9,7 @@ import Foundation
 import ArgumentParser
 import UniformTypeIdentifiers
 
-struct TypeCommands: ParsableCommand {
+struct TypeCommands: AsyncParsableCommand {
 
   static let configuration = CommandConfiguration(
     commandName: "type",
@@ -38,14 +38,14 @@ struct TypeCommands: ParsableCommand {
     var bundleID = false
   }
   
-  struct Get: ParsableCommand {
+  struct Get: AsyncParsableCommand {
     static let configuration
     = CommandConfiguration(abstract: "Get the path to the default application.")
     
     @OptionGroup var utidentifier: UTIdentifier
     @OptionGroup var bundleID: IdentifierFlag
     
-    func run() {
+    func run() async {
       guard let appURL = LSKit.defaultAppURL(forTypeIdentifier: utidentifier.value) else {
         print("<no default app found>")
         return
@@ -61,14 +61,14 @@ struct TypeCommands: ParsableCommand {
     }
   }
   
-  struct List: ParsableCommand {
+  struct List: AsyncParsableCommand {
     static let configuration
     = CommandConfiguration(abstract: "List all applications that can handle this type identifier.")
     
     @OptionGroup var utidentifier: UTIdentifier
     @OptionGroup var bundleID: IdentifierFlag
 
-    func run() {
+    func run() async {
       let appURLs = LSKit.appURLs(forTypeIdentifier: utidentifier.value)
       
       for appURL in appURLs {
@@ -85,15 +85,15 @@ struct TypeCommands: ParsableCommand {
     }
   }
   
-  struct Set: ParsableCommand {
+  struct Set: AsyncParsableCommand {
     static let configuration
     = CommandConfiguration(abstract: "Set the default app for this type identifier.")
     
     @OptionGroup var utidentifier: UTIdentifier
     @Argument var identifier: String
     
-    func run() {
-      let result = LSKit.setDefaultApp(identifier: identifier, forTypeIdentifier: utidentifier.value)
+    func run() async {
+      let result = await LSKit.setDefaultApp(identifier: identifier, forTypeIdentifier: utidentifier.value)
       
       if result == 0 {
         print("set \(identifier) for \(utidentifier.value)")
@@ -104,13 +104,13 @@ struct TypeCommands: ParsableCommand {
     }
   }
   
-  struct FileExtensions: ParsableCommand {
+  struct FileExtensions: AsyncParsableCommand {
     static let configuration
     = CommandConfiguration(abstract: "prints the file extensions for the given type identifier")
     
     @OptionGroup var utidentifier: UTIdentifier
     
-    func run() {
+    func run() async {
       guard let utype = UTType(utidentifier.value) else {
         print("<none>")
         TypeCommands.exit(withError: ExitCode(3))
@@ -121,13 +121,13 @@ struct TypeCommands: ParsableCommand {
     }
   }
 
-  struct Info: ParsableCommand {
+  struct Info: AsyncParsableCommand {
     static let configuration
     = CommandConfiguration(abstract: "prints information for the given type identifier")
 
     @OptionGroup var utidentifier: UTIdentifier
     
-    func run() {
+    func run() async {
       guard let utype = UTType(utidentifier.value) else {
         print("<none>")
         TypeCommands.exit(withError: ExitCode(3))
