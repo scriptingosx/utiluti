73999167819df07e20e85e6dea9a77bddc222566
Armin Briegel <1933192+scriptingosx@users.noreply.github.com>
2026-03-15 19:15:11 +0100

    for file type files and preferences, you can set default apps for file extensions with the `extension:`prefix

--- Sources/utiluti/ManageCommand.swift
+++ Sources/utiluti/ManageCommand.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import ArgumentParser
+import UniformTypeIdentifiers
 
 struct ManageCommand: AsyncParsableCommand {
   static let configuration = CommandConfiguration(
@@ -49,17 +50,27 @@ struct ManageCommand: AsyncParsableCommand {
 
   func manageTypes(types: [String:Any]) async throws {
     for (uti, value) in types {
+      var uti = uti
+      let display = uti
+      
       guard let bundleID = value as? String
       else {
         if verbose { print("skipping non-string value '\(value)' for \(uti)")}
         continue
       }
+      
+      if uti.hasPrefix("extension:") {
+        let fileExt = String(uti.dropFirst(10))
+        if let utype = UTType(filenameExtension: fileExt) {
+          uti = utype.identifier
+        }
+      }
 
       let result = await LSKit.setDefaultApp(identifier: bundleID, forTypeIdentifier: uti)
       if result == 0 {
-        print("set \(bundleID) for \(uti)")
+        print("set \(bundleID) for \(display)")
       } else {
-        print("ERROR: cannot set \(bundleID) for \(uti) (error \(result))")
+        print("ERROR: cannot set \(bundleID) for \(display) (error \(result))")
       }
     }
   }
